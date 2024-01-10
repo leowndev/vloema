f13c02827d6610007e7aeb49921933cb08d8159f
leowndev <leonardogt2308@gmail.com>
2024-01-10 10:24:35 -0300

    Setup pugPlugin on viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import path from "path";
 import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
 
 export default defineConfig({
   root: "src",
   build: {
     outDir: path.join(__dirname, "dist"),
   },
+
+  plugins: [pugPlugin()],
 });
